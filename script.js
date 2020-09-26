eae0710909ef9649bf4870f9704db062bbeb4d47
Juan Pablo Velasquez Camargo <61720010+JP-Velasquez@users.noreply.github.com>
2020-09-26 14:13:59 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Ejercicio 3
+var boton = document.getElementById("btnEnviarDatos");
+boton.addEventListener("click", ejercicio3);
+
+function ejercicio3 (){
+    alert("Htpa, porfin funcione como debe ser");
+    
+  }
